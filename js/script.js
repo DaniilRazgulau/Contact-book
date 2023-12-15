b0c6a0755f6d59d9ac25abbdf555a2fda4b4f50a
Razgulau Daniil <razgulaudaniil@gmail.com>
2023-12-15 16:10:26 +0300

    completed script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
- //     new  User ({
-            //     id: '1',
-            //     name: "dan",
-            //     phone: "+37529 222 22 22", 
-            //     email: "dan@test.by",
-            //     addres: "Minsk"
-            // }),
-
-            // new  User({
-            //     id: '2',
-            //     name: "Max",
-            //     phone: "+37529 222 1112", 
-            //     email: "max@test.by",
-            //     addres: "New York"
-            // })
-
 class User {
     constructor(data) {
         this.data = data;
@@ -34,13 +18,27 @@ class User {
 
 class Contacts {
     constructor(){
-        this.contactsData = this.createUserFromLocalStorage() ?? [];
+        this.contactsData = []
     }
+
+    //  createCookie(){
+    //     let days = 10;
+    //     let seconds = days*24*60*60;
+    //     document.cookie = `storageExpiration=${JSON.stringify(this.contactsData)}; max-age=${seconds}`        
+    // }
+
+    // deleteLocalStorage(){
+    //     if(document.cookie.length === 0) {
+    //         localStorage.clear()
+    //     } 
+    // }
 
     createUserFromLocalStorage(){
         const localStorageData = JSON.parse(localStorage.getItem('contactsItem'));
 
         if(!localStorageData){
+            const data = this.getData();
+            console.log(data);
             return undefined;
         }
 
@@ -51,8 +49,48 @@ class Contacts {
         return userFromLocalStorage;
     }
 
+    async getValidateContactData(){
+        const isContactsCookie = !!this.getCookie('contactCookie');
+
+        if(isContactsCookie){
+            return this.createUserFromLocalStorage() ?? [];
+        }
+        localStorage.removeItem('contactsItem');
+        return await this.getData();
+    }
+
     setLocaleStorage(){
         localStorage.setItem('contactsItem', JSON.stringify(this.contactsData));
+        this.setCookie();
+    }
+
+    getCookie(name) {
+        let matches = document.cookie.match(new RegExp(
+          "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    async getData(){
+        try{
+            const responce = await fetch('https://jsonplaceholder.typicode.com/users'); 
+            if(responce.status >= 400 && responce.status <= 420 || responce.status >= 500 && responce.status <= 520){
+                throw new Error('error')
+            }
+            const ServerData = await responce.json();
+            const data = this.serverDataMapper(ServerData);
+            return data
+        } catch(error){
+            console.error(error);        
+        }
+    }
+
+    serverDataMapper(data){
+        return data = data.map(({address: {city, street}, name, phone, email, id}) => new User({address: `${city}  ${street}`, name, phone, email, id: `${id}`}));
+    }
+
+    setCookie(){
+        document.cookie =`contactCookie = contacts; max-age = ${25}`;
     }
 
     add(userData){
@@ -91,8 +129,21 @@ class ContactApp extends Contacts{
         this.app = this.createRootElement();
         document.body.appendChild(this.app);
         this.addContactEvent();
-        this.get();
+        this.startApp();
         this.self = this;
+    }
+
+    startApp(){
+        const preloader = document.createElement('div');
+        preloader.classList.add('preloader');
+        preloader.innerHTML = `<h2 class="preloader__text">Data is loading ...</h2>`;
+        document.body.appendChild(preloader);
+        this.getValidateContactData().then((data) => {
+            preloader.remove();
+            this.contactsData = data;
+            this.setLocaleStorage();
+            this.get();
+        });
     }
 
     createRootElement(){
@@ -107,7 +158,7 @@ class ContactApp extends Contacts{
                             <input tupe="text" class="contact__name" placeholder="Имя">
                             <input tupe="text" class="contact__phone" placeholder="Телефон">
                             <input tupe="email" class="contact__email" placeholder="Email">
-                            <input tupe="text" class="contact__addres" placeholder="Адрес">
+                            <input tupe="text" class="contact__address" placeholder="Адрес">
                             <button class="contact__btn">Добавить контакт</button>
                         </div>
                     </div>
@@ -129,7 +180,7 @@ class ContactApp extends Contacts{
         const name = document.querySelector('.contact__name');
         const phone = document.querySelector('.contact__phone');
         const email = document.querySelector('.contact__email');
-        const addres = document.querySelector('.contact__addres');
+        const addres = document.querySelector('.contact__address');
         console.log( addres.value )
 
         const userData = {
@@ -164,13 +215,13 @@ class ContactApp extends Contacts{
         let contactList = '';
 
         getContactsData.forEach(({data}) => {
-            const {name, phone, email, id, addres} = data;
+            const {name, phone, email, id, address} = data;
             contactList += `
                 <li class="item">
                     <div class="item__name">Имя: ${name}</div>
                     <div class="item__phone">Телефон: ${phone}</div>
                     <div class="item__email">Email: ${email}</div>
-                    <div class="item__address">Адрес: ${addres}</div>
+                    <div class="item__address">Адрес: ${address}</div>
                     <div class="item__btn">
                         <button class="btn__delete" id="${id}">Удалить</button>
                         <button class="btn__edit" data-edit="${id}">Редактирование</button>
@@ -324,3 +375,63 @@ const app = new ContactApp();
 
 // console.log(usersData);
 // console.log(usersData.a2);
+
+
+
+
+
+
+// const f1 = () => {
+//     console.log('f1');                  PROMISE
+// }
+
+// const f3 = () => {
+//     console.log('f3');
+// }
+
+// f1();
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('f2');
+//         resolve('Успешно');
+//     }, 2000)    
+// }).then((result) => {
+//     console.log(result);
+//     f3()
+// }).catch((error) => {
+//     console.log(error);
+// }).finally(() => {
+//     console.log('Запустим в любом случае');
+// })
+
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((responce) => responce.json());
+// .then((data) => {
+//     console.log(data);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+
+// const getData = async() => {
+//     try{
+
+//     const responce = await fetch('https://jsonplaceholder.typicode.com/users1');
+//     console.log(responce);
+//     if(responce.status === 404){
+//         throw new Error('endpoint not found')
+//     }
+//     const data = await responce.json();
+//     console.log(data);
+//     } catch(error){
+//         console.log(error);
+//     }
+//     finally{
+//         console.log('bla');
+//     }
+// }
+
+// getData();
